e60803a80aa903516d801b0a9376e106db0a08cd
Thai Phan <thai.phan@invenco.com>
2024-03-04 09:26:50 +1300

    lspconfig add other language servers

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,3 +5,10 @@ lsp_zero.on_attach(function(client, bufnr)
     -- to learn the available actions
     lsp_zero.default_keymaps({buffer = bufnr})
 end)
+
+local lsp_config = require('lspconfig')
+lsp_config.clangd.setup{}
+lsp_config.cmake.setup{}
+lsp_config.pyright.setup{}
+lsp_config.bashls.setup{}
+
