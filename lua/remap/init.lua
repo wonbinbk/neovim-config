53df778b5c048e3fc5ae5f4a649c72b9a2c5cd74
Thai Phan <thai.phan@invenco.com>
2024-03-04 11:09:41 +1300

    Change keymap C-R to C-e so we can still use C-R for undo

--- lua/remap/init.lua
+++ lua/remap/init.lua
@@ -7,7 +7,7 @@ vim.keymap.set("n", "<C-k>", "<cmd>lua require('fzf-lua').builtin()<CR>", {})
 vim.keymap.set("n", "<C-l>", "<cmd>lua require('fzf-lua').live_grep_glob()<CR>", {})
 vim.keymap.set("n", "<C-g>", "<cmd>lua require('fzf-lua').grep_project()<CR>", {})
 vim.keymap.set("n", "<F1>", "<cmd>lua require('fzf-lua').help_tags()<CR>", {})
-vim.keymap.set("n", "<C-r>", "<cmd>lua require('fzf-lua').resume()<CR>", {})
+vim.keymap.set("n", "<C-e>", "<cmd>lua require('fzf-lua').resume()<CR>", {})
 
 -- turn high light off after search
 vim.keymap.set("n", "<CR>", "<cmd>noh<CR>", {})
