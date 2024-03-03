6a98e3324a3708b397a291766e1c62f14d60a9ff
Thai Phan <thai.phan@invenco.com>
2024-03-04 01:24:00 +1300

    Add colors.lua

--- lua/remap/init.lua
+++ lua/remap/init.lua
@@ -9,3 +9,5 @@ vim.keymap.set("n", "<C-g>", "<cmd>lua require('fzf-lua').grep_project()<CR>", {
 vim.keymap.set("n", "<F1>", "<cmd>lua require('fzf-lua').help_tags()<CR>", {})
 vim.keymap.set("n", "<C-r>", "<cmd>lua require('fzf-lua').resume()<CR>", {})
 
+-- turn high light off after search
+vim.keymap.set("n", "<CR>", "<cmd>noh<CR>", {})
