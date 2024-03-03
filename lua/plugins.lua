3d4864df247c25f1b5d65606d163ae4713611d26
Thai Phan <thai.phan@invenco.com>
2024-03-04 02:06:14 +1300

    Add lsp-zero

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -30,10 +30,22 @@ return {
 	{ 
 		"sainnhe/gruvbox-material", priority = 1000 , config = true, opts = ...
 	},
+    {
+        "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {},
+    },
 	{
 		"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
 	},
     {
         "tpope/vim-fugitive"
-    }
+    },
+    -- LSP-zero from VonHeikemen/lsp-zero.nvim
+    -- Uncomment the two plugins below if you want to manage the language servers from neovim
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
 }
