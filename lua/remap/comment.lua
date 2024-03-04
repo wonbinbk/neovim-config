d7b8048bb843085c5ed3d0724d19c26672096c1f
Thai Phan <thai.phan@invenco.com>
2024-03-04 18:11:56 +1300

    Split mapping to different modules for ease of maintenance

--- lua/remap/comment.lua
+++
@@ -1,1 +0,0 @@
-vim.keymap.set("n", "<C-c>", "gcc<CR>", {})
--- lua/remap/tabvsspace.lua
+++ lua/remap/tabvsspace.lua
@@ -1,7 +1,7 @@
 -- tab and spaces
 vim.keymap.set("n", "<F2>", "<cmd>set wrap!<CR>", {})
 -- toggle expandtab
-vim.keymap.set("n", "<F3>", function()
+vim.keymap.set("n", "<F9>", function()
     local expandtab = vim.opt.expandtab:get()
     vim.opt.expandtab = not expandtab
     if vim.opt.expandtab:get() then
@@ -10,4 +10,4 @@ vim.keymap.set("n", "<F3>", function()
         print("Using tabs")
     end
 end, {silent=true, noremap=true, desc="Toggle expandtab and echo status"})
-vim.keymap.set("x", "<F4>", ":retab!<CR>", {})
+vim.keymap.set("x", "<F10>", ":retab!<CR>", {})
