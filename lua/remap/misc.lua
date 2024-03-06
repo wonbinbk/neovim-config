522ac0e0742908ce6d01fa9ae63e341b06ad2bcb
Thai Phan <thai.phan@invenco.com>
2024-03-06 15:18:53 +1300

    Add Comment plugin and keymap to navigate between windows

---
+++ lua/remap/misc.lua
@@ -0,0 +1,21 @@
+-- turn high light off after search
+vim.keymap.set("n", "<leader><CR>", "<cmd>noh<CR>", {})
+
+-- moving around split windows
+vim.keymap.set("n", "<C-h>", "<cmd>wincmd h<CR>", {})
+vim.keymap.set("n", "<C-j>", "<cmd>wincmd j<CR>", {})
+vim.keymap.set("n", "<C-k>", "<cmd>wincmd k<CR>", {})
+vim.keymap.set("n", "<C-l>", "<cmd>wincmd l<CR>", {})
+
+--------------------------
+-- in tabvsspace.lua
+-- F2 to swap
+-- F3 to toggle expandtab
+-- F4 to retab
+--------------------------
+-- F5 to toggle displaying white spaces characters
+vim.keymap.set("n", "<F5>", "<cmd>set list!<CR>", {})
+-- F6 to remove trailing white spaces
+vim.keymap.set("n", "<F6>", "<cmd>%s/\\s\\+$//e<CR>", { silent = true })
+-- F8 to switch source header
+vim.keymap.set("n", "<F8>", "<cmd>ClangdSwitchSourceHeader<CR>", { silent = true })
