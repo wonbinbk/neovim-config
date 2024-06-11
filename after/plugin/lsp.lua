1c28a67d246a1802084a57901aaf09bbef8fc9e7
wonbinbk <thaiphd@gmail.com>
2024-06-11 12:31:46 +1200

    Add setup for Mason otherwise it won't be enabled after Neovim started.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -15,3 +15,4 @@ lsp_config.bashls.setup{}
 -- disable lsp diagnostic by override this with a do-nothing function
 -- uncomment if we want to disable diagnostics
 -- vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
+require("mason").setup()
