52c94c927d56dfd16ba8793aa362ae3361314555
wonbinbk <thaiphd@gmail.com>
2024-12-24 14:26:49 +1300

    Improve resize_windows.lua
    - Allow user to specify % for a percentage or bare number for absolute
      value
    - Also validate the input argument.

--- lua/user_defined/resize_windows.lua
+++ lua/user_defined/resize_windows.lua
@@ -1,19 +1,53 @@
 -- Resizes
+
+local function parse_args(args)
+    -- parse args and return if it a percentage and the actual number
+    if not args or not string.match(args, "^%d+%%?$") then
+        print("Invalid argument: " .. args)
+        return false,nil
+    end
+    if args:sub(-1) == "%" then
+        local percent = args:sub(1, -2)
+        if tonumber(percent) > 100 then
+            return true, nil
+        end
+        return true, percent
+    else
+        return false, args
+    end
+end
+
 vim.api.nvim_create_user_command("Vr", function(opts)
     local usage = "Usage: [VirticalResize] :Vr {number (%)}"
-    if not opts.args or not string.len(opts.args) == 2 then
+    local percentage, number = parse_args(opts.args)
+    if number == nil then
         print(usage)
         return
     end
-    vim.cmd(":vertical resize " .. vim.opt.columns:get() * (opts.args / 100.0))
+    local new_size
+    -- the below if block can be written shortly using Lua idiom
+    -- new_size = percentage and vim.opt.columns:get() * (number / 100.0) or number
+    -- however it will be very obscure for non Lua devs
+    if percentage then
+        new_size = vim.opt.columns:get() * (number / 100.0)
+    else
+        new_size = number
+    end
+    vim.cmd(":vertical resize " .. new_size)
 end, { nargs = "*" })
 
 vim.api.nvim_create_user_command("Hr", function(opts)
     local usage = "Usage: [HorizontalResize] :Hr {number (%)}"
-    if not opts.args or not string.len(opts.args) == 2 then
+    local percentage, number = parse_args(opts.args)
+    if number == nil then
         print(usage)
         return
     end
-    vim.cmd(":resize " .. ((vim.opt.lines:get() - vim.opt.cmdheight:get()) * (opts.args / 100.0))
-    )
+    local new_size
+    if percentage then
+        new_size = (vim.opt.lines:get() - vim.opt.cmdheight:get()) * (number / 100.0)
+    else
+        new_size = number
+    end
+    vim.cmd(":resize " .. new_size)
 end, { nargs = "*" })
